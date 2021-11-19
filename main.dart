fceea867d286185686ce1e1b9d1cfdcd05fa7878
FirdausHaireen <69784466+FirdausHaireen@users.noreply.github.com>
2021-11-20 02:06:10 +0800

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,65 +1,85 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(const MyApp());
 
-class MyApp extends StatefulWidget{
-
-  State<StatefulWidget> createState(){
-    return MyAppState();
-  }
-
-}
-
-class MyAppState extends State <MyApp>{
-
-  final myController = TextEditingController();
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Pick Your Choice',
+      home: MyCustomChoice(),
+    );
+  }
+}
+
+class MyCustomChoice extends StatefulWidget {
+  const MyCustomChoice({Key? key}) : super(key: key);
+
+  @override
+  _MyCustomChoiceState createState() => _MyCustomChoiceState();
+}
+
+class _MyCustomChoiceState extends State<MyCustomChoice> {
+  final myController = TextEditingController();
+  
+  
+  @override
   void dispose() {
-    // Clean up the controller when the widget is disposed.
     myController.dispose();
     super.dispose();
   }
-  
+
   @override
-  var questions = ['This is my question','This is my second question?','This is my third question?','This is my forth question?'];
-  Widget build(BuildContext context){
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Assignment 1'),
+  var answer = ['Durian', 'Rambutan', 'Apple'];
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pick a number from 1-3'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: TextField(
+          controller: myController,
         ),
-        body: Column(
-          children: [
-            TextField(
-              controller: myController,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Choose a number from 1-3'
-              ),
-            ),
+      ),
 
-            ElevatedButton(child: Text('Answer 1'), onPressed:(){
-
-                showDialog(
-                  context: context,
-                  builder: (context) {
-                    return AlertDialog(
-                    // Retrieve the text the user has entered by using the
-                    // TextEditingController.
-                      content: Text(myController.text),
-                   );
-                  },
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+      
+          if (myController.text == '1'){
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: Text(answer[0]),
                 );
-              } 
-            
-            ),
-          ]
-        ),
+              });
+          }
+
+          else if (myController.text == '2'){
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: Text(answer[1]),
+                );
+              });
+          }
+
+          else if (myController.text == '3'){
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: Text(answer[2]),
+                );
+              });
+          }
+        },
+        tooltip: 'Press to show value',
+        child: const Icon(Icons.send),
       ),
     );
   }
